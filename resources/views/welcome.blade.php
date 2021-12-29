7665ae0bbaf617c3c8cb8a2b04c3d91e18148c06
abdelrhmanfahmi <abdo.fahmy9696@gmail.com>
2021-12-29 20:36:03 +0200

    sayed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link href="assets/css/theme.css" rel="stylesheet" type="text/css" media="all" />
-        <link rel="stylesheet" href="{{asset('public/css/app.css')}}">
+        <link rel="stylesheet" href="https://rezoe.herokuapp.com//css/app.css">
     </head>
     <body>
         <div id="app">
